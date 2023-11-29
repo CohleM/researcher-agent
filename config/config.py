248dacec0f3b17aec1f1c932246468e3e7239cc4
CohleM <manish981194@gmail.com>
2023-11-29 17:34:33 +0545

    add config file

---
+++ config/config.py
@@ -0,0 +1,12 @@
+class Config:
+    def __init__(self):
+
+        self.retriever = "Weaviate"
+        self.search_engine = "duckduckgo"
+        self.llm = "gpt-3.5-turbo"
+        self.max_search_query = 3
+        self.max_search_results_per_query = 5
+        self.total_words = 1000
+        
+        
+
